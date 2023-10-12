b7399e31dcf38fb39a80217c18fba34de8c18efc
Mana <monokos31@gmail.com>
2023-10-12 21:33:40 +0900

    libft

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mosada <mosada@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/27 10:42:41 by mosada            #+#    #+#             */
+/*   Updated: 2023/10/07 15:42:53 by mosada           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*a;
+	size_t	count1;
+	size_t	count2;
+
+	if (!s1 || !s2)
+		return (NULL);
+	count1 = ft_strlen(s1);
+	count2 = ft_strlen(s2);
+	a = (char *)malloc(sizeof(char) * (count1 + count2 + 1));
+	if (!a)
+		return (NULL);
+	ft_strlcpy(a, s1, count1 + 1);
+	ft_strlcpy(a + count1, s2, count2 + 1);
+	return (a);
+}
